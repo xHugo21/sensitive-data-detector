316aec178979dd8ca09c97797fa1bf63c0bdcae7
xHugo21 <hugo2121h@gmail.com>
2025-11-21 15:47:09 +0100

    fix: proxy script reference now is properly found

--- proxy/app/main.py
+++ proxy/app/main.py
@@ -20,7 +20,7 @@ def run_proxy():
         "--set",
         "confdir=~/.mitmproxy",
         "--scripts",
-        "app.sensitive_data_detector",
+        "app/sensitive_data_detector.py",
     ]
 
     print(f"Starting mitmproxy on {PROXY_HOST}:{PROXY_PORT}")
